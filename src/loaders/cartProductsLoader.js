b0581608c28e2e32d30c7a02f841f9fa6a07692b
Aminul667 <rahathssn@gmail.com>
2023-05-17 12:54:13 +0600

    72-8 (Advanced) Use Post To Load Some Products Using Keys client

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,23 +1,33 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
-    const products = await loadedProducts.json();
+  // if cart data is in the database then we have to use async await
+  const storedCart = getShoppingCart();
+  const ids = Object.keys(storedCart);
+  console.log(ids);
 
-    // if cart data is in the database then we have to use async await
-    const storedCart = getShoppingCart();
-    const savedCart = [];
+  const loadedProducts = await fetch("http://localhost:5000/productsById", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(ids),
+  });
 
-    for(const id in storedCart){
-        const addedProduct = products.find(pd => pd._id === id);
-        if(addedProduct){
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            savedCart.push(addedProduct);
-        }
+  const products = await loadedProducts.json();
+
+  const savedCart = [];
+
+  for (const id in storedCart) {
+    const addedProduct = products.find((pd) => pd._id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
     }
+  }
 
-    return savedCart;
-}
+  return savedCart;
+};
 
 export default cartProductsLoader;
